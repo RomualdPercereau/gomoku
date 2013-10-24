fe217bca227d4c419908ff372b7db1360fed9eba
Romuald Percereau <romuald@Romualds-MacBook-Pro.local>
2013-10-24 18:23:27 +0200

    Ajout de jade, correction du mot de passe root pour sql

--- nodejs/web.js
+++ nodejs/web.js
@@ -14,7 +14,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'koink',
+  password : 'root',
 });
 
 connection.connect();
