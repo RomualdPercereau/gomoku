8a9a0cb0b56a5c5c6aa0c84cb44f628e7c20a823
Romuald Percereau <romuald@Romualds-MacBook-Pro.local>
2013-11-04 17:47:26 +0100

    Gestion des clics sur les cases + box de choix

--- nodejs/projet/public/javascripts/all.js
+++ nodejs/projet/public/javascripts/all.js
@@ -3,21 +3,45 @@ $(function() {
 
 if (location.pathname == "/game")
 {
-	console.log("koink");
+	mode = ""
+
+  $(function() {
+    $( "#dialog-confirm" ).dialog({
+    	title: "Vous jouez ? ",
+      resizable: false,
+      height:140,
+      modal: true,
+      buttons: {
+        "1 vs 1": function() {
+          $( this ).dialog( "close" );
+          mode = "PVP";
+        },
+        "Joueur contre IA": function() {
+          $( this ).dialog( "close" );
+          mode = "PVI";
+
+        }
+      }
+    });
+  });
+
+
+
 	$(".line li").click(function()
 	{
-		console.log(this.id);
-
-
-		console.log("ok");
-
-		$.getJSON( "arbitre/" + this.id, function( data )
+		console.log("go" + mode);
+		id = this.id.split("-")[1];
+		$.getJSON("arbitre/" + id, function(data)
 		{
 			console.log(data)
+			$("#id-" + id).append("<div id='circle-" + id+ "' class='circle'></div>");
+			$("#circle-" + id).css("background", data['color']);
+			$("#current_player").html(mode)
 		});
-
-
 	})
+
+
+
 }
 
 })
