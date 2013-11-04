5d02a2c84331a604f7b67727005d2aa3c0f926f7
Romuald Percereau <romuald@Romualds-MacBook-Pro.local>
2013-11-04 14:01:11 +0100

    json

--- nodejs/projet/public/javascripts/all.js
+++ nodejs/projet/public/javascripts/all.js
@@ -6,7 +6,14 @@ if (location.pathname == "/game")
 	console.log("koink");
 	$(".line li").click(function()
 	{
-		console.log(this.id)
+		console.log(this.id);
+
+
+
+		$.getJSON( "ajax/test.json", function( data ) {
+		});
+
+
 	})
 }
 
