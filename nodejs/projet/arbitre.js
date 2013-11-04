a9f622556f81d11fa173f97ffe00a3ce86e74fa6
AntoineHabert <antoinehabert@yahoo.fr>
2013-11-04 17:30:17 +0100

    envoie de la map vide en json

--- nodejs/projet/arbitre.js
+++ nodejs/projet/arbitre.js
@@ -4,13 +4,32 @@ exports.simple_test = function(req) {
 
 	if(req.session.lastPage)
 		new_user = false;
+
+	if (new_user) {
+		var map = new Array();
+		var i = 0;
+		var j;
+		while (i < 16)
+		{
+			map[i] = new Array();
+			j = 0;
+			while (j < 16)
+			{
+				map[i][j] = 0;
+				j++;
+			}
+			i++;
+		}
+		req.session.map = map;
+	}
+
 	req.session.lastPage = '/arbitre';
 
 	var test = req.params.case_id;
 
 	var json = {'id' : test,
 				'color' : '#FF00FF',
-				'new_user' : new_user
+				'map' : map
 				};
 				return (json);
 }
