5256f36b1431291e13a271ed43bab7da2b4e1b34
Romuald Percereau <romuald@Romualds-MacBook-Pro.local>
2013-11-04 18:20:16 +0100

    Gestion des cases coulorés

--- nodejs/projet/public/javascripts/all.js
+++ nodejs/projet/public/javascripts/all.js
@@ -4,6 +4,7 @@ $(function() {
 if (location.pathname == "/game")
 {
 	mode = ""
+	id_player = 0;
 
   $(function() {
     $( "#dialog-confirm" ).dialog({
@@ -31,16 +32,54 @@ if (location.pathname == "/game")
 	{
 		console.log("go" + mode);
 		id = this.id.split("-")[1];
-		$.getJSON("arbitre/" + id, function(data)
-		{
+		$.getJSON("arbitre/" + id_player + "/" + id, function(data)
+		{			// $("#id-" + id).append("<div id='circle-" + id+ "' class='circle'></div>");
+
+			id_player = (id_player + 1) %2
 			console.log(data)
-			$("#id-" + id).append("<div id='circle-" + id+ "' class='circle'></div>");
-			$("#circle-" + id).css("background", data['color']);
+			refreshMap(data['map'])
+			// $("#circle-" + id).css("background", data['color']);
 			$("#current_player").html(mode)
 		});
 	})
+	$("#restart").click(function()
+	{
+		document.cookie = "connect.sid=" +escape(-1);
+		window.location.href=window.location.href
+		return (false)
+	})
 
 
+	function refreshMap(map)
+	{
+		for (var i = map.length - 1; i >= 0; i--) {
+			if (map[i] == 1)
+			{
+				if ($("#circle-" + i).html() === undefined)
+				{
+					$("#id-" + i).append("<div id='circle-" + i+ "' class='circle'></div>");
+					$("#circle-" + i).css("background", "green");
+				}
+			}
+			if (map[i] == 2)
+			{
+				if ($("#circle-" + i).html() === undefined)
+				{
+					$("#id-" + i).append("<div id='circle-" + i+ "' class='circle'></div>");
+					$("#circle-" + i).css("background", "#FF00FF");
+				}
+			}
+
+			if (map[i] == 3)
+			{
+				if ($("#circle-" + i).html() === undefined)
+				{
+					$("#id-" + i).append("<div id='circle-" + i+ "' class='circle'></div>");
+					$("#circle-" + i).css("background", "red");
+				}
+			}
+		};
+	}
 
 }
 
